283f67765691e48a347e86c416fcf0a5bcc4f606
Keshav Raju <44800315+KeshavRajuR@users.noreply.github.com>
2020-10-20 13:28:40 +0530

    Update create.sh

--- General/maybe-here/create.sh
+++ General/maybe-here/create.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#run the script with SUDO privileges
+
+mkdir maybe_here
+cd maybe_here
+
 for folder in {1..50}
 do
 	mkdir $folder
@@ -13,5 +18,13 @@ do
 done
 
 cd 32
+chmod 644 maybe_here_13
 echo "flag{1_c4n_f1nd_4ny_f1l3}" > maybe_here_13
 chmod 444 maybe_here_13
+cd ..
+
+zip -qq -r maybe_here.zip *
+chmod 664 maybe_here.zip
+mv maybe_here.zip ..
+cd ..
+rm -r maybe_here
